5fd8f3b484eaadfede5eb90feb82ec603821b022
doerlbh <doerlbh@gmail.com>
2017-05-25 15:10:20 -0700

    update code

--- raspberry_pi/spec_analysis_baihan.sh
+++ raspberry_pi/spec_analysis_baihan.sh
@@ -8,7 +8,7 @@
 
 folder=`pwd`
 mscript=${folder//*\//}_spec_analysis_baihan.m
-echo "% spectogram analysis.m for experiment ${folder//*\//} \n% Baihan Lin, Apr 2017\n" > $mscript
+echo "% spectogram analysis.m for experiment ${folder//*\//} \n% Baihan Lin, May 2017\n" > $mscript
 
 echo "clear all; close all;" >> $mscript
 echo "\nload('${folder//*\//}.mat')\n" >> $mscript
@@ -19,44 +19,44 @@ do
     acc0=${f//.csv/}
     acc1=${acc0/acc0/acc1}
 
-	echo "%% -----$acc0 $acc1------\n" >> $mscript
-	echo "size = length($acc0);\n" >> $mscript
-	echo "plane0 = var($acc0);\n" >> $mscript
-	echo "plane1 = var($acc1);\n" >> $mscript
-	echo "s_$acc0=zeros(size,1);\n" >> $mscript
-	echo "s_$acc1=zeros(size,1);\n\n" >> $mscript
+	echo "%% -----$acc0 $acc1------" >> $mscript
+	echo "size = length($acc0);" >> $mscript
+	echo "plane0 = var($acc0);" >> $mscript
+	echo "plane1 = var($acc1);" >> $mscript
+	echo "s_$acc0=zeros(size,1);" >> $mscript
+	echo "s_$acc1=zeros(size,1);" >> $mscript
 	
-	echo "parfor n = 1:size\n" >> $mscript
-    	echo "s_$acc0(n) = $acc0(n,:)*plane0.'/norm(plane0,2);\n" >> $mscript
-    	echo "s_$acc1(n) = $acc1(n,:)*plane1.'/norm(plane1,2);\n" >> $mscript
-	echo "end\n\n" >> $mscript
+	echo "for n = 1:size" >> $mscript
+    	echo "s_$acc0(n) = $acc0(n,:)*plane0.'/norm(plane0,2);" >> $mscript
+    	echo "s_$acc1(n) = $acc1(n,:)*plane1.'/norm(plane1,2);" >> $mscript
+	echo "end\n" >> $mscript
 
-	echo "nwin = 100;\n" >> $mscript
-	echo "wind = kaiser(nwin,10);\n" >> $mscript
-	echo "nlap = nwin-10;\n" >> $mscript
-	echo "nfft = 1024;\n" >> $mscript
-	echo "Fs = 1/0.0003;\n\n" >> $mscript
+	echo "nwin = 100;" >> $mscript
+	echo "wind = kaiser(nwin,10);" >> $mscript
+	echo "nlap = nwin-10;" >> $mscript
+	echo "nfft = 1024;" >> $mscript
+	echo "Fs = 1/0.0003;\n" >> $mscript
 
-	echo "figure(1)\n" >> $mscript
-	echo "spectrogram(s_$acc0,wind,nlap,nfft,Fs,'yaxis')\n" >> $mscript
-	echo "title('Spectogram ${acc0//_/\\_}');\n" >> $mscript
-	echo "filename = strcat(path, 'spec_$acc0.png');\n" >> $mscript
-	echo "saveas(gcf, filename,'png');\n" >> $mscript
-	echo "close(figure(1));\n\n" >> $mscript
+	echo "figure(1)" >> $mscript
+	echo "spectrogram(s_$acc0,wind,nlap,nfft,Fs,'yaxis')" >> $mscript
+	echo "title('Spectogram ${acc0//_/\\_}');" >> $mscript
+	echo "filename = strcat(path, 'spec_$acc0.png');" >> $mscript
+	echo "saveas(gcf, filename,'png');" >> $mscript
+	echo "close(figure(1));\n" >> $mscript
 
-	echo "figure(2)\n" >> $mscript
-	echo "spectrogram(s_$acc1,wind,nlap,nfft,Fs,'yaxis')\n" >> $mscript
-	echo "title('Spectogram ${acc1//_/\\_}');\n" >> $mscript
-	echo "filename = strcat(path, 'spec_$acc1.png');\n" >> $mscript
-	echo "saveas(gcf, filename,'png');\n" >> $mscript
-	echo "close(figure(2));\n" >> $mscript
+	echo "figure(2)" >> $mscript
+	echo "spectrogram(s_$acc1,wind,nlap,nfft,Fs,'yaxis')" >> $mscript
+	echo "title('Spectogram ${acc1//_/\\_}');" >> $mscript
+	echo "filename = strcat(path, 'spec_$acc1.png');" >> $mscript
+	echo "saveas(gcf, filename,'png');" >> $mscript
+	echo "close(figure(2));" >> $mscript
 
-	echo "figure(3)\n" >> $mscript
-	echo "spectrogram(s_$acc0./s_$acc1,wind,nlap,nfft,Fs,'yaxis')\n" >> $mscript
-	echo "title('Ratio Spectogram acc0${acc1//_/\\_}');\n" >> $mscript
-	echo "filename = strcat(path, 'spec_ratio_acc0$acc1.png');\n" >> $mscript
-	echo "saveas(gcf, filename,'png');\n" >> $mscript
-	echo "close(figure(3));\n" >> $mscript
+	echo "figure(3)" >> $mscript
+	echo "spectrogram(s_$acc0./s_$acc1,wind,nlap,nfft,Fs,'yaxis')" >> $mscript
+	echo "title('Ratio Spectogram acc0${acc1//_/\\_}');" >> $mscript
+	echo "filename = strcat(path, 'spec_ratio_acc0$acc1.png');" >> $mscript
+	echo "saveas(gcf, filename,'png');" >> $mscript
+	echo "close(figure(3));" >> $mscript
 done
 
 #fscript=`pwd`/$mscript
