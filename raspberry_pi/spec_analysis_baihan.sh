d7f07d63d759b772c345148b493b4724f43df9da
doerlbh <doerlbh@gmail.com>
2017-05-25 00:40:37 -0700

    update code

---
+++ raspberry_pi/spec_analysis_baihan.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# -*- coding: utf-8 -*-
+# Code: spectogram analysis
+# Author: Baihan Lin
+# Date: Apr 2017
+# Lab: UbiComp Lab
+
+folder=`pwd`
+mscript=${folder//*\//}_spec_analysis_baihan.m
+echo "% spectogram analysis.m for experiment ${folder//*\//} \n% Baihan Lin, Apr 2017\n" > $mscript
+
+echo "clear all; close all;" >> $mscript
+echo "\nload('${folder//*\//}.mat')\n" >> $mscript
+echo "path=('$folder/spectogram/');\nsystem(['mkdir ' path]);\n" >> $mscript
+
+for f in `ls acc0*csv`
+do
+    acc0=${f//.csv/}
+    acc1=${acc0/acc0/acc1}
+
+	echo "%% -----$acc0 $acc1------\n" >> $mscript
+	echo "size = length($acc0);\n" >> $mscript
+	echo "plane0 = var($acc0);\n" >> $mscript
+	echo "plane1 = var($acc1);\n" >> $mscript
+	echo "s_$acc0=zeros(size,1);\n" >> $mscript
+	echo "s_$acc1=zeros(size,1);\n\n" >> $mscript
+	
+	echo "for n = 1:size\n" >> $mscript
+    	echo "s_$acc0(n) = $acc0(n,:)*plane0.'/norm(plane0,2);\n" >> $mscript
+    	echo "s_$acc1(n) = $acc1(n,:)*plane1.'/norm(plane1,2);\n" >> $mscript
+	echo "end\n\n" >> $mscript
+
+	echo "nwin = 100;\n" >> $mscript
+	echo "wind = kaiser(nwin,10);\n" >> $mscript
+	echo "nlap = nwin-10;\n" >> $mscript
+	echo "nfft = 1024;\n" >> $mscript
+	echo "Fs = 1/0.0003;\n\n" >> $mscript
+
+	echo "figure(1)\n" >> $mscript
+	echo "spectrogram(s_$acc0,wind,nlap,nfft,Fs,'yaxis')\n" >> $mscript
+	echo "title('Spectogram\_04\_24\_00\_04\_56');\n" >> $mscript
+	echo "filename = strcat(path, 'Spectogram_04_24_00_04_56.png');\n" >> $mscript
+	echo "saveas(gcf, filename,'png');\n" >> $mscript
+	echo "close(figure(1));\n" >> $mscript
+done
+
+#fscript=`pwd`/$mscript
+
+/Applications/Work/MATLAB_R2015b.app/bin/matlab -nodesktop -r "${mscript//.m/}"
--- raspberry_pi/specgram_baihan.py
+++ raspberry_pi/specgram_baihan.py
@@ -5,31 +5,37 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import sys
+from itertools import islice
 
 dt = 0.0003
-allTime=66.0
-sampleSize=220000
+allTime = 66.0
+sampleSize = 220001
 
 for arg in sys.argv:
-	filename=arg
+	filename = arg
 
+x=[]
+y=[]
+z=[]
 with open(filename) as myfile:
-    data = [next(myfile) for x in xrange(sampleSize)]
+    data = list(islice(myfile, sampleSize))
     for line in data:
-        print(line.split()[0])
+        x.append(line.split()[0])
+        y.append(line.split()[1])
+        z.append(line.split()[2])
 
 t = np.arange(0.0, allTime, dt)
-s1 = np.sin(2*np.pi*100*t)
-s2 = 2*np.sin(2*np.pi*400*t)
+#s1 = np.sin(2*np.pi*100*t)
+#s2 = 2*np.sin(2*np.pi*400*t)
 
 # create a transient "chirp"
-mask = np.where(np.logical_and(t > 10, t < 12), 1.0, 0.0)
-s2 = s2 * mask
+#mask = np.where(np.logical_and(t > 10, t < 12), 1.0, 0.0)
+#s2 = s2 * mask
 
 # add some noise into the mix
-nse = 0.01*np.random.random(size=len(t))
+#nse = 0.01*np.random.random(size=len(t))
 
-x = s1 + s2 + nse  # the signal
+#x = s1 + s2 + nse  # the signal
 NFFT = 1024       # the length of the windowing segments
 Fs = int(1.0/dt)  # the sampling frequency
 
@@ -43,15 +49,15 @@ plt.plot(t, x)
 plt.subplot(234, sharex=ax1)
 Pxx, freqs, bins, im = plt.specgram(x, NFFT=NFFT, Fs=Fs, noverlap=900)
 
-ax1 = plt.subplot(232)
-plt.plot(t, x)
-plt.subplot(235, sharex=ax1)
-Pxx, freqs, bins, im = plt.specgram(x, NFFT=NFFT, Fs=Fs, noverlap=900)
+ax2 = plt.subplot(232)
+plt.plot(t, y)
+plt.subplot(235, sharex=ax2)
+Pxx, freqs, bins, im = plt.specgram(y, NFFT=NFFT, Fs=Fs, noverlap=900)
 
-ax1 = plt.subplot(233)
-plt.plot(t, x)
-plt.subplot(236, sharex=ax1)
-Pxx, freqs, bins, im = plt.specgram(x, NFFT=NFFT, Fs=Fs, noverlap=900)
+ax3 = plt.subplot(233)
+plt.plot(t, z)
+plt.subplot(236, sharex=ax3)
+Pxx, freqs, bins, im = plt.specgram(z, NFFT=NFFT, Fs=Fs, noverlap=900)
 
-plt.show()
 plt.savefig(filename.replace("csv", "png"))
+plt.show()
