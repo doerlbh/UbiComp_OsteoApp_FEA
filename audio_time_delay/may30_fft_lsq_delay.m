1f7699e1ba7e5d73f7fdb0e95fde25926b9f3eaf
doerlbh <doerlbh@gmail.com>
2017-06-07 13:31:22 -0700

    update code

--- audio_time_delay/may30_fft_lsq_delay.m
+++ audio_time_delay/may30_fft_lsq_delay.m
@@ -5,17 +5,16 @@ clear all; close all;
 
 path='/Users/DoerLBH/Dropbox/git/UbiComp_OsteoApp_FEA/audio_time_delay/may30/';
 
-%%
-
 [y,fs] = audioread(strcat(path,'DR0000_0017.wav'));
 
-y = y(:,1);
 figure(1);
 dt = 1/fs;
 t = 0:dt:(length(y)*dt)-dt;
 subplot(2,2,1); plot(t,y(:,1)); xlabel('Seconds'); ylabel('Amplitude');title('ch1');
 subplot(2,2,2); plot(t,y(:,2)); xlabel('Seconds'); ylabel('Amplitude');title('ch2');
-subplot(2,2,3); plot(t,fft(y(:,1))); xlabel('Seconds'); ylabel('Amplitude');title('ch1_fft');
-subplot(2,2,4); plot(t,fft(y(:,2))); xlabel('Seconds'); ylabel('Amplitude');title('ch2_fft');
+subplot(2,2,3); plot(t,fft(y(:,1))); xlabel('Seconds'); ylabel('Amplitude');title('ch1-fft');
+subplot(2,2,4); plot(t,fft(y(:,2))); xlabel('Seconds'); ylabel('Amplitude');title('ch2-fft');
+
+
 
 
